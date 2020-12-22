3ddb9b3a07b1a3adf33948e27a5dc56f8db989fa
GeoffAtHome <geoff@soord.org.uk>
2020-12-22 18:04:25 +0000

    Inprove documentation and testing.

--- src/edit-map.ts
+++ src/edit-map.ts
@@ -12,7 +12,7 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import { LitElement, html, customElement, property, css, PropertyValues, internalProperty } from 'lit-element';
+import { LitElement, html, customElement, property, css, PropertyValues, internalProperty, query, TemplateResult } from 'lit-element';
 import load from './maploader'
 
 
@@ -20,6 +20,9 @@ let map: google.maps.Map;
 /**
  * Interface onto the Google Maps API
  *
+ * @fires modifiedPolygon -  Dispatched when editing selected polygon has finished.
+ * @fires clickedPolygon -  Dispatched when a polygon has been 'clicked'
+ * 
  */
 @customElement('edit-map')
 export class EditMap extends LitElement {
@@ -31,30 +34,58 @@ export class EditMap extends LitElement {
     #mapid {
       height: 100%;
     }
+
+    #popup {
+      display: none;
+      position:absolute;
+      background-color: blue;
+      border-radius: 5px;
+      box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+      padding: 10px;
+    }
+
+        #popup[active] {
+      display: block;
+      background-color: pink;
+    }
   `;
 
+  @query('#popup')
+  private popup: HTMLElement | undefined
+
   /**
- * The Google map options
- */
-  @property({ type: Object })
-  options = {};
+    * The Google map options
+    * @type { google.maps.MapOptions }
+    */
+  @property({ attribute: true, type: Object })
+  public options: google.maps.MapOptions = {};
 
   /**
    * The polygons to draw.
    * Each polygon has a key, path and options
    */
-  @property({ type: Object })
-  polygonData: PolygonData | undefined
+  @property({ attribute: 'polygonData', type: Object })
+  public polygonData: PolygonData | undefined
 
   /**
    * Polygon to edit
-   * pc: string - index to polygon
-   * state: boolean 
-   * - true to edit
-   * - false stop editing and fire modified polygon
+   * 
+   * @type: {pc: string; state: boolean }
    */
-  @property({ type: Object })
-  editPolygon: { pc: string; state: boolean } = { pc: '', state: false }
+  @property({ attribute: 'editPolygon', type: Object })
+  public editPolygon: { pc: string; state: boolean } = { pc: '', state: false }
+
+  /**
+   * ShowPopup: show or hide
+   */
+  @internalProperty()
+  private showPopup = ''
+
+  /**
+   * popupText: HTML to display
+   */
+  @internalProperty()
+  private popupText: TemplateResult = html``
 
   /**
    * The polygons that have been drawn on the map so that these can be modified.
@@ -65,6 +96,7 @@ export class EditMap extends LitElement {
   render() {
     return html`
       <div id="mapid"></div>
+      <span id="popup" ?active="${this.showPopup === 'show'}">${this.popupText}</span>
     `;
   }
 
@@ -96,7 +128,6 @@ export class EditMap extends LitElement {
 
   /**
    * modified polygon event.
-   *
    * @event edit-map#modifiedPolygon
    * @type {object}
    * @property {string} pc - id of polygon
@@ -132,12 +163,27 @@ export class EditMap extends LitElement {
 
         google.maps.event.addListener(newPolygon, "dblclick", (event) => { this.removeVertex(event, newPolygon) })
         google.maps.event.addListener(newPolygon, "click", (_event) => { this.clickedPolygon(pc, newPolygon) })
+        google.maps.event.addListener(newPolygon, "mouseover", (event) => { this.mouseoverPolygon(event.domEvent, pc) })
+        google.maps.event.addListener(newPolygon, "mouseout", (_event) => { this.mouseoutPolygon() })
         newPolygon.setMap(map);
         this.setPolygonEditMode({ pc: pc, state: false })
-
       }
     }
     return true
+  }
+
+  mouseoverPolygon(event: MouseEvent, text: string) {
+    if (this.popup) {
+      this.showPopup = 'show'
+      this.popupText = html`<h2>Hello</h2>${text}`
+      this.popup.style.left = event.pageX.toString() + 'px'
+      this.popup.style.top = event.pageY.toString() + 'px'
+      console.log(this.popup.style.left)
+    }
+  }
+
+  mouseoutPolygon() {
+    this.showPopup = 'hide'
   }
 
   removeVertex(event: { vertex: number | undefined }, polygon: google.maps.Polygon) {
